c79050571d971f02c46adbf372f491a5d32a9d04
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-05 14:59:04 +0200

    fix my header

--- binary_trees.h
+++ binary_trees.h
@@ -28,10 +28,10 @@ typedef struct binary_tree_s
 /*--------------------------------------------------------------------*/
 
 void	binary_tree_print(const binary_tree_t *);
+void    binary_tree_delete(binary_tree_t *tree);
 void	binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void	binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void	binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-void	binary_tree_delete(binary_tree_t *tree);
 
 /*--------------------------------------------------------------------*/
 
