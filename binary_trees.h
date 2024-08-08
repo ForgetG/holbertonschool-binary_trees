def992f340276e52d6c3f7c9efd7b51b4a993082
Morg92b <morgan.bouaziz@outlook.fr>
2024-08-08 12:09:59 +0200

    add my function on .h

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ void	binary_tree_levelorder(const binary_tree_t *tree,
 /*--------------------------------------------------------------------*/
 
 binary_tree_t	*binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t	*binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t	*binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t	*binary_tree_insert_right(binary_tree_t *parent, int value);
 binary_tree_t	*binary_tree_sibling(binary_tree_t *node);
