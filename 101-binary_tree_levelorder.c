7db68f2fd05259a9c770f2b568dcc5c49243bdc9
Forget Guillaume <9430@holbertonstudents.com>
2024-08-08 14:06:48 +0200

    modified index value + loop run condition from 1 to height + 1 in binary_tree_levelorder function

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,5 +1,48 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: it's pointer to the root node of the tree to measure the height
+ * Return: height of tree or 0 if it's NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
+ * process_level - Processes nodes at a given level.
+ * @tree: Pointer to the root node of the tree.
+ * @level: Level to process.
+ * @func: Pointer to a function to call for each node.
+ */
+void process_level(const binary_tree_t *tree, size_t level, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (level == 1)
+		func(tree->n);
+	else if (level > 1)
+	{
+		process_level(tree->left, level - 1, func);
+		process_level(tree->right, level - 1, func);
+	}
+}
+
 /**
  * binary_tree_levelorder - Traverses a binary tree,
  * Using level-order traversal.
@@ -9,40 +52,12 @@
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t **queue, **new_queue, *node;
-	size_t front = 0, back = 0, size = 1024;
+	size_t	height, index;
 
 	if (tree == NULL || func == NULL)
 		return;
 
-	queue = malloc(sizeof(*queue) * size);
-	if (queue == NULL)
-		return;
-
-	queue[back++] = (binary_tree_t *)tree;
-
-	while (front < back)
-	{
-		node = queue[front++];
-		func(node->n);
-
-		if (node->left != NULL)
-			queue[back++] = node->left;
-		if (node->right != NULL)
-			queue[back++] = node->right;
-
-		if (back >= size)
-		{
-			size *= 2;
-			new_queue = realloc(queue, sizeof(*queue) * size);
-			if (new_queue == NULL)
-			{
-				free(queue);
-				return;
-			}
-			queue = new_queue;
-		}
-	}
-
-	free(queue);
+	height = binary_tree_height(tree);
+	for (index = 1; index <= height + 1; index++)
+		process_level(tree, index, func);
 }
